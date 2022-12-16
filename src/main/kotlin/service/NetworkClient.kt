9524a9df9095d692618bfb4a0f7f040b6fff5540
Tobias Grabert <tobias.grabert@tu-dortmund.de>
2022-12-16 15:41:18 +0100

    network implementation

--- src/main/kotlin/service/NetworkClient.kt
+++ src/main/kotlin/service/NetworkClient.kt
@@ -1,8 +1,11 @@
 package service
 
 import service.message.GameInitMessage
+import service.message.TurnMessage
 import tools.aqua.bgw.core.BoardGameApplication
 import tools.aqua.bgw.net.client.BoardGameClient
+import tools.aqua.bgw.net.client.NetworkLogging
+import tools.aqua.bgw.net.common.annotations.GameActionReceiver
 import tools.aqua.bgw.net.common.notification.PlayerJoinedNotification
 import tools.aqua.bgw.net.common.notification.PlayerLeftNotification
 import tools.aqua.bgw.net.common.response.CreateGameResponse
@@ -14,65 +17,113 @@ class NetworkClient(playerName: String,
                     host: String,
                     secret: String,
                     var networkService: NetworkService,
-) : BoardGameClient(playerName, host, secret) {
+) : BoardGameClient(playerName, host, secret, networkLoggingBehavior = NetworkLogging.VERBOSE) {
 
     var sessionID: String? = null
 
     override fun onJoinGameResponse(response: JoinGameResponse) {
-        check(networkService.connectionState == ConnectionState.WAITING_FOR_JOIN_CONFIRMATION)
-            {"unexpected join confirmation"}
+        if (networkService.connectionState == ConnectionState.WAITING_FOR_JOIN_CONFIRMATION) {
+            println("received unexpected join confirmation")
+        }
 
         when (response.status) {
             JoinGameResponseStatus.SUCCESS -> {
-                sessionID = response.sessionID
                 networkService.updateConnectionState(ConnectionState.WAITING_FOR_INIT)
-                println("successfully joined game: ${sessionID}, current players are: ${response.opponents}")
+                println("Successfully joined game.")
+                println("The host's greeting is \"${response.message}\".")
             }
+            JoinGameResponseStatus.ALREADY_ASSOCIATED_WITH_GAME ->
+                error("You are already in a game.")
+
+            JoinGameResponseStatus.INVALID_SESSION_ID ->
+                error("SessionID invalid.")
+
+            JoinGameResponseStatus.PLAYER_NAME_ALREADY_TAKEN ->
+                error("Player name already exists.")
+
+            JoinGameResponseStatus.SERVER_ERROR ->
+                error(response)
             else -> disconnect()
         }
-
     }
 
     override fun onCreateGameResponse(response: CreateGameResponse) {
-        check(networkService.connectionState == ConnectionState.WAITING_FOR_HOST_CONFIRMATION)
-            {"unexpected host confirmation"}
+        if (networkService.connectionState == ConnectionState.WAITING_FOR_HOST_CONFIRMATION) {
+            println("unexpected host confirmation")
+        }
 
         when (response.status) {
             CreateGameResponseStatus.SUCCESS -> {
+                println("Successfully created game.")
                 networkService.updateConnectionState(ConnectionState.WAITING_FOR_PLAYERS)
-                sessionID = response.sessionID
-                println("successfully created game: $sessionID")
             }
+
+            CreateGameResponseStatus.ALREADY_ASSOCIATED_WITH_GAME ->
+                error("Leave current game first.")
+
+            CreateGameResponseStatus.SESSION_WITH_ID_ALREADY_EXISTS ->
+                error("Session id already exists.")
+
+            CreateGameResponseStatus.GAME_ID_DOES_NOT_EXIST ->
+                error("GameID does not exist.")
+
+            CreateGameResponseStatus.SERVER_ERROR ->
+                error(response)
             else -> disconnect()
         }
     }
 
     override fun onPlayerJoined(notification: PlayerJoinedNotification) {
-        check(networkService.connectionState == ConnectionState.WAITING_FOR_PLAYERS)
-            {"unexpected player joined notification"}
+        if(networkService.connectionState != ConnectionState.WAITING_FOR_INIT) {
+            if (networkService.connectionState == ConnectionState.WAITING_FOR_PLAYERS) {
+                println("unexpected player joined notification")
+            }
 
-        println("${notification.sender} joined the game.")
-        println("${notification.sender}: ${notification.message}")
-        networkService.joinedPlayers.add(notification.sender)
+            println("${notification.sender} joined the game.")
+            println("${notification.sender}: ${notification.message}")
 
-        networkService.updateConnectionState(ConnectionState.READY_FOR_GAME)
+            networkService.joinedPlayers.add(notification.sender)
+
+            if (networkService.joinedPlayers.size > 6) {
+                networkService.updateConnectionState(ConnectionState.WAITING_FOR_PLAYERS)
+                println("Too many players are connected to the game. Currently Connected Players: ${networkService.joinedPlayers.size}")
+                return
+            }
+
+            networkService.updateConnectionState(ConnectionState.READY_FOR_GAME)
+        } else {
+            println("A Player joined the remotely hosted game")
+        }
     }
 
     override fun onPlayerLeft(notification: PlayerLeftNotification) {
-        if (networkService.joinedPlayers.size < 2) {
-            networkService.updateConnectionState(ConnectionState.WAITING_FOR_PLAYERS)
+        if (networkService.connectionState != ConnectionState.WAITING_FOR_INIT) {
+            if (networkService.connectionState == ConnectionState.READY_FOR_GAME) {
+                networkService.joinedPlayers.remove(notification.sender)
+                if (networkService.joinedPlayers.size < 2) {
+                    networkService.updateConnectionState(ConnectionState.WAITING_FOR_PLAYERS)
+                }
+                println("Player: ${notification.sender} has left the game. Current Players: ${networkService.joinedPlayers.size}")
+            }
+            // TODO inGame boolean, for stopping game if player disconnects during a game
+            if(false) {
+                println("a player has left the game. disconnecting.")
+                disconnect()
+            }
         }
-        // TODO inGame boolean, for stopping game if player disconnects during a game
-        if(true) {
-            println("a player has left the game. disconnecting.")
-            disconnect()
-        }
-        println("a player has left the game. current player amount: ${networkService.joinedPlayers.size}")
     }
 
+    @GameActionReceiver
     fun onInitReceived(message: GameInitMessage, sender: String) {
         BoardGameApplication.runOnGUIThread {
             networkService.startNewJoinedGame(message)
+        }
+    }
+
+    @GameActionReceiver
+    fun onTurnMessageReceived(message: TurnMessage, sender: String) {
+        BoardGameApplication.runOnGUIThread {
+            //execute turn
         }
     }
 
--- src/main/kotlin/service/NetworkService.kt
+++ src/main/kotlin/service/NetworkService.kt
@@ -1,87 +1,95 @@
 package service
 
 import entity.Player
-import service.RootService
 import service.message.*
 
 class NetworkService(var rootService: RootService): AbstractRefreshingService() {
     var connectionState: ConnectionState = ConnectionState.DISCONNECTED
-    val SERVER_ADDRESS = "sopra.cs.tu-dortmund.de:80/bgw-net/connect"
-    val GAME_ID = "CableCar"
+    private val SERVER_ADDRESS = "sopra.cs.tu-dortmund.de:80/bgw-net/connect"
+    private val GAME_ID = "CableCar"
 
-    var client: NetworkClient? = null
+    private var client: NetworkClient? = null
 
-    var joinedPlayers = mutableListOf<String>()
-    fun joinGame(secret: String, name:String, sessionID: String) {
-        if(connect(secret, name)) {
-            client?.joinGame(sessionID, "Hallo von Gruppe 10.")
-            updateConnectionState(ConnectionState.WAITING_FOR_JOIN_CONFIRMATION)
-        }
-    }
 
-    fun hostGame(secret: String, name: String, sessionID: String) {
-        connect(secret, name)
-        if(connect(secret, name)) {
+    fun hostGame(secret: String, playerName: String, sessionID: String) {
+        if(connect(secret, playerName)) {
             client?.createGame(GAME_ID, sessionID, "Hallo von Gruppe 10")
             updateConnectionState(ConnectionState.WAITING_FOR_HOST_CONFIRMATION)
         }
     }
 
-    private fun connect(secret: String, name: String): Boolean {
+    var joinedPlayers = mutableListOf<String>()
+    fun joinGame(secret: String, name:String, sessionID: String) {
+        if(connect(secret, name)) {
+            client?.joinGame(sessionID, "Hallo von Gruppe 10.")
+        }
+    }
+
+
+    /**
+     * Opens a connection to the Server via creating the client
+     */
+    private fun connect(secret: String, playerName: String): Boolean {
         require(connectionState == ConnectionState.DISCONNECTED && client == null)
         { "already connected to another game" }
 
         require(secret.isNotBlank()) { "server secret must be given" }
-        require(name.isNotBlank()) { "player name must be given" }
+        require(playerName.isNotBlank()) { "player name must be given" }
 
         val newClient =
             NetworkClient(
-                playerName = name,
+                playerName = playerName,
                 host = SERVER_ADDRESS,
                 secret = secret,
                 networkService = this
             )
 
-        return if (newClient.connect()) {
+        if(newClient.connect()) {
             this.client = newClient
             updateConnectionState(ConnectionState.CONNECTED)
-            true
-        } else {
-            false
+            return true
         }
+        return false
     }
 
-    fun sendGameInitMessage(message: GameInitMessage) {
-        require(connectionState == ConnectionState.READY_FOR_GAME) {"Not enough Players have joined yet."}
-        client?.sendGameActionMessage(message)
+    fun sendGameInitMessage(message: GameInitMessage?): Boolean {
+        if (connectionState != ConnectionState.READY_FOR_GAME) {
+            if (connectionState == ConnectionState.WAITING_FOR_PLAYERS) {
+                println("Not enough or too many Players have joined the Game. Current Amount: ${joinedPlayers.size}")
+            }
+            return false
+        }
+        if (message != null) {
+            client?.sendGameActionMessage(message)
+        } else {
+            // Create GameInitMessage
+            val playerInfoList = mutableListOf<PlayerInfo>()
+            for (player in joinedPlayers) {
+                playerInfoList.add(PlayerInfo(player, PlayerType.HUMAN))
+            }
+
+            // TODO get TileSupply list from Entity
+            val gameInitMessage = GameInitMessage(
+                rotationAllowed = true,
+                players = playerInfoList,
+                tileSupply = listOf()
+            )
+            client?.sendGameActionMessage(gameInitMessage)
+        }
         updateConnectionState(ConnectionState.GAME_INITIALIZED)
+        return true
     }
 
     fun sendTurnMessage(message: TurnMessage) {
-
+        // TODO
     }
 
     fun startNewJoinedGame(message: GameInitMessage) {
-        val players = mutableListOf<Player>()
-        for (player in message.players) {
-            players.add(Player(player.name, mutableListOf()))
-        }
-        rootService.gameService.startNewGame(players)
-        updateConnectionState(ConnectionState.GAME_INITIALIZED)
+        // TODO
     }
 
     fun startNewHostedGame(localPlayers: List<Player>, rotationAllowed: Boolean) {
-        // creating GameInitMessage
-        var players = mutableListOf<PlayerInfo>()
-        for (lp in localPlayers) {
-            players.add(PlayerInfo(lp.name, PlayerType.HUMAN))
-        }
-        var initMessage = GameInitMessage(rotationAllowed, players, listOf<Tile>())
-
-        // TODO adding Tiles from Entity to GameInitMessage
-
-        // sending GameInitMessage
-        sendGameInitMessage(initMessage)
+        // TODO
     }
 
     fun updateConnectionState(newState: ConnectionState) {
