fa4341d84e8637554c5c3acfc5831fb622962a3e
413413-beep <leonard.sander@udo.edu>
2022-12-16 11:06:26 +0100

    Changes by poudy

--- src/main/kotlin/view/GameOverScene.kt
+++ src/main/kotlin/view/GameOverScene.kt
@@ -1,6 +1,44 @@
 package view
 
+import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.core.BoardGameScene
+import tools.aqua.bgw.util.Font
+import tools.aqua.bgw.visual.ColorVisual
+import tools.aqua.bgw.visual.CompoundVisual
+import tools.aqua.bgw.visual.TextVisual
+import java.awt.Color
 
 class GameOverScene : BoardGameScene(1920,1080) {
+
+    private val restartButton = Button(width = 223, height = 70, posX = 181, posY = 720,
+        visual = CompoundVisual(
+            ColorVisual.WHITE.apply { transparency = 0.3 },
+            TextVisual(
+                font = Font(size = 60, color = Color.RED, family = "Calibri", fontStyle = Font.FontStyle.ITALIC),
+                text = "Restart Game"
+            )
+        )
+    )
+
+    private val mainMenuButton = Button(width = 223, height = 70, posX =181, posY = 823,
+        visual = CompoundVisual(
+            ColorVisual.WHITE.apply { transparency = 0.3 },
+            TextVisual(
+                font = Font(size = 60, color = Color.RED, family = "Calibri", fontStyle = Font.FontStyle.ITALIC),
+                text = "Main Menu"
+            )
+        )
+    )
+
+    private val quitButton = Button(width = 223, height = 70, posX = 181, posY = 823,
+        visual = CompoundVisual(
+            ColorVisual.WHITE.apply { transparency = 0.3 },
+            TextVisual(
+                font = Font(size = 60, color = Color.RED, family = "Calibri", fontStyle = Font.FontStyle.ITALIC),
+                text = "Quit"
+            )
+        )
+    )
+
+
 }
--- src/main/kotlin/view/NotificationGameScene.kt
+++ src/main/kotlin/view/NotificationGameScene.kt
@@ -3,4 +3,6 @@ package view
 import tools.aqua.bgw.core.MenuScene
 
 class NotificationGameScene : MenuScene(1920,360) {
+
+
 }
