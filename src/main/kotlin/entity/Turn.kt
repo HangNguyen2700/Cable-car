3a3b9be58760dfe06a053d2dde53e428d3aeb793
413413-beep <leonard.sander@udo.edu>
2022-12-21 14:49:09 +0100

    update entity nochma

--- src/main/kotlin/entity/Turn.kt
+++ src/main/kotlin/entity/Turn.kt
@@ -4,6 +4,6 @@ data class Turn(val gameField: GameField, val players: MutableList<Player>) {
 
     var previousTurn : Turn? = null
     var nextTurn : Turn? = null
-    var playerIndex : Int? = null
+    var currentPlayerIndex : Int? = null
 
 }
