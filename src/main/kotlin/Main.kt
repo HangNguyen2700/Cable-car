29ac531cf7df2e2eed5c1257681464fe531abf79
Jonah Hadt <jonahhadt@outlook.de>
2023-01-15 15:10:28 +0100

    Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-import view.CCApplication
+import service.RootService
 
 fun main() {
-    println("Das Sopra ist voll cool")
-    CCApplication().show()
-    println("Application ended. Goodbye")
+
+    //val rootService = RootService()
+    //rootService.gameService.startNewGame(listOf("Martin", "Werner"))
+
+
+
 }
