19a46c3fc1415152b2b0b259f6f8117df280911c
413413-beep <leonard.sander@udo.edu>
2023-01-16 18:27:00 +0100

    shuffle turn order for hotseat

--- src/main/kotlin/view/CCApplication.kt
+++ src/main/kotlin/view/CCApplication.kt
@@ -85,17 +85,31 @@ class CCApplication : BoardGameApplication("Carbel Car Game") {
             showAndStoreMenuScene(mainMenuScene, 3000)
         }
         startGameButton.onMouseClicked = {
-            this@CCApplication.rootService.gameService.startNewGame(
-                listOfNotNull(
-                    mainMenuScene.nameField.text,
-                    this.nameFields[0].text,
-                    if (this.nameFields[1].text != "") this.nameFields[1].text else null,
-                    if (this.nameFields[2].text != "") this.nameFields[2].text else null,
-                    if (this.nameFields[3].text != "") this.nameFields[3].text else null,
-                    if (this.nameFields[4].text != "") this.nameFields[4].text else null,
-                ),
-                isLocalOnlyGame = true, isHostedGame = false
-            )
+            if (shuffleTurnOrderCheckbox.isChecked) {
+                this@CCApplication.rootService.gameService.startNewGame(
+                    listOfNotNull(
+                        mainMenuScene.nameField.text,
+                        this.nameFields[0].text,
+                        if (this.nameFields[1].text != "") this.nameFields[1].text else null,
+                        if (this.nameFields[2].text != "") this.nameFields[2].text else null,
+                        if (this.nameFields[3].text != "") this.nameFields[3].text else null,
+                        if (this.nameFields[4].text != "") this.nameFields[4].text else null,
+                    ).shuffled(),
+                    isLocalOnlyGame = true, isHostedGame = false
+                )
+            } else {
+                this@CCApplication.rootService.gameService.startNewGame(
+                    listOfNotNull(
+                        mainMenuScene.nameField.text,
+                        this.nameFields[0].text,
+                        if (this.nameFields[1].text != "") this.nameFields[1].text else null,
+                        if (this.nameFields[2].text != "") this.nameFields[2].text else null,
+                        if (this.nameFields[3].text != "") this.nameFields[3].text else null,
+                        if (this.nameFields[4].text != "") this.nameFields[4].text else null,
+                    ),
+                    isLocalOnlyGame = true, isHostedGame = false
+                )
+            }
         }
     }
 
