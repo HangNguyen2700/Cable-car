1934314e8a9e277dc8b7d10d1a6aedbd3dc6e3a9
413413-beep <leonard.sander@udo.edu>
2023-01-20 22:36:21 +0100

    add rootService to GameOverScene constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 import service.RootService
+import view.CCApplication
 
 fun main() {
+
+    println("Das Sopra ist voll cool")
+    CCApplication().show()
+    println("Application ended. Goodbye")
+
 
     //val rootService = RootService()
     //rootService.gameService.startNewGame(listOf("Martin", "Werner"))
