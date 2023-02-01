bf0d0794431f2ba610a1686d5e772fe5b20d3f6f
nguyenhangnguyen <laniday2711@gmail.com>
2023-02-01 11:10:36 +0100

    fewer warnings

--- src/main/kotlin/service/NetworkClient.kt
+++ src/main/kotlin/service/NetworkClient.kt
@@ -1,12 +1,12 @@
 package service
 
 
-import edu.udo.cs.sopra.ntf.GameInitMessage
-import edu.udo.cs.sopra.ntf.TurnMessage
+//import edu.udo.cs.sopra.ntf.GameInitMessage
+//import edu.udo.cs.sopra.ntf.TurnMessage
 import tools.aqua.bgw.core.BoardGameApplication
 import tools.aqua.bgw.net.client.BoardGameClient
 import tools.aqua.bgw.net.client.NetworkLogging
-import tools.aqua.bgw.net.common.annotations.GameActionReceiver
+//import tools.aqua.bgw.net.common.annotations.GameActionReceiver
 import tools.aqua.bgw.net.common.notification.PlayerJoinedNotification
 import tools.aqua.bgw.net.common.notification.PlayerLeftNotification
 import tools.aqua.bgw.net.common.response.CreateGameResponse
@@ -31,7 +31,7 @@ class NetworkClient(playerName: String,
                     var networkService: NetworkService,
 ) : BoardGameClient(playerName, host, secret, networkLoggingBehavior = NetworkLogging.VERBOSE) {
 
-    var sessionID: String? = null
+//    var sessionID: String? = null
 
     override fun onJoinGameResponse(response: JoinGameResponse) {
         if (networkService.connectionState == ConnectionState.WAITING_FOR_JOIN_CONFIRMATION) {
@@ -141,91 +141,91 @@ class NetworkClient(playerName: String,
         }
     }
 
-    /**
-     * On init received
-     *
-     * @param message
-     * @param sender
-     */
-    @GameActionReceiver
-    fun onInitReceived(message: GameInitMessage, sender: String) {
-        if (networkService.connectionState == ConnectionState.WAITING_FOR_INIT) {
-            BoardGameApplication.runOnGUIThread {
-                networkService.startNewJoinedGame(message)
-            }
-        }
-    }
-
-    /**
-     * On turn message received
-     *
-     * @param message
-     * @param sender
-     */
-    @GameActionReceiver
-    fun onTurnMessageReceived(message: TurnMessage, sender: String) {
-        BoardGameApplication.runOnGUIThread {
-            if (sender == networkService.rootService.currentGame!!.currentTurn.
-                players[networkService.rootService.currentGame!!.currentTurn.currentPlayerIndex].name) {
-                // check gameState
-                // check placedTiles
-                val ePlacedTilesSize = networkService.rootService.currentGame!!.currentTurn.gameField.tiles.size
-                val mPlacedTilesSize = message.gameStateVerificationInfo.placedTiles.size
-                if (ePlacedTilesSize != mPlacedTilesSize) {
-                    println("PlacedTiles in Entity is not same size as PlacedTiles in Received TurnMessage." +
-                            " Size of Entity PlacedTiles:" +
-                            " $ePlacedTilesSize, Size of TurnMessage PlacedTiles: $mPlacedTilesSize")
-                } else {
-                    for (i in 0 until message.gameStateVerificationInfo.placedTiles.size) {
-                        val eID = networkService.rootService.currentGame!!.currentTurn.gameField.tiles[i].id
-                        val mID = message.gameStateVerificationInfo.placedTiles[i].id
-                        if (eID != mID) {
-                            println("PlacedTiles with Index: $i does not have the same ID." +
-                                    " ID of Entity Tile: $eID, ID of TurnMessage Tile: $mID")
-                        }
-                    }
-                }
-                // check TileStack/Supply
-                val eTileStackSize = networkService.rootService.currentGame!!.currentTurn.
-                                        gameField.tileStack.tiles.size
-                val mTileStackSize = message.gameStateVerificationInfo.supply.size
-                if (eTileStackSize != mTileStackSize) {
-                    println("TileStack in Entity is not the same size as Supply in Received TurnMessage." +
-                            " Size of Entity TileStack: $eTileStackSize, " +
-                            "Size of TurnMessage TileStack: $mTileStackSize")
-                } else {
-                    for (i in 0 until mTileStackSize) {
-                        val eStackID = networkService.rootService.currentGame!!.currentTurn.
-                                        gameField.tileStack.tiles[i].id
-                        val mStackID = message.gameStateVerificationInfo.supply[i]
-                        if (eStackID != mStackID) {
-                            println("TileID in TileStack does not equal ID in Received TurnMessage. " +
-                                    "Index: $i, Entity ID: $eStackID, TurnMessage ID: $mStackID")
-                        }
-                    }
-                }
-                // check playerScores
-                val ePlayers = networkService.rootService.currentGame!!.currentTurn.players
-                for (i in 0 until ePlayers.size) {
-                    if (ePlayers[i].score != message.gameStateVerificationInfo.playerScores[i]) {
-                        println("PlayerScore of ${ePlayers[i].name} " +
-                                "does not equal score from Received TurnMessage. " +
-                                "Entity Score: ${ePlayers[i].score}," +
-                                " TurnMessage Score: ${message.gameStateVerificationInfo.playerScores[i]}")
-                    }
-                }
-
-                // placeTile
-                networkService.rootService.playerActionService.
-                    placeTile(!message.fromSupply, message.posX,
-                        message.posY, message.rotation/90, true)
-
-            } else {
-                println("Received TurnMessage from $sender, even though it is not their turn.")
-            }
-            //next player gets called by playerActionServices
-        }
-    }
+//    /**
+//     * On init received
+//     *
+//     * @param message
+//     * @param sender
+//     */
+//    @GameActionReceiver
+//    fun onInitReceived(message: GameInitMessage, sender: String) {
+//        if (networkService.connectionState == ConnectionState.WAITING_FOR_INIT) {
+//            BoardGameApplication.runOnGUIThread {
+//                networkService.startNewJoinedGame(message)
+//            }
+//        }
+//    }
+//
+//    /**
+//     * On turn message received
+//     *
+//     * @param message
+//     * @param sender
+//     */
+//    @GameActionReceiver
+//    fun onTurnMessageReceived(message: TurnMessage, sender: String) {
+//        BoardGameApplication.runOnGUIThread {
+//            if (sender == networkService.rootService.currentGame!!.currentTurn.
+//                players[networkService.rootService.currentGame!!.currentTurn.currentPlayerIndex].name) {
+//                // check gameState
+//                // check placedTiles
+//                val ePlacedTilesSize = networkService.rootService.currentGame!!.currentTurn.gameField.tiles.size
+//                val mPlacedTilesSize = message.gameStateVerificationInfo.placedTiles.size
+//                if (ePlacedTilesSize != mPlacedTilesSize) {
+//                    println("PlacedTiles in Entity is not same size as PlacedTiles in Received TurnMessage." +
+//                            " Size of Entity PlacedTiles:" +
+//                            " $ePlacedTilesSize, Size of TurnMessage PlacedTiles: $mPlacedTilesSize")
+//                } else {
+//                    for (i in 0 until message.gameStateVerificationInfo.placedTiles.size) {
+//                        val eID = networkService.rootService.currentGame!!.currentTurn.gameField.tiles[i].id
+//                        val mID = message.gameStateVerificationInfo.placedTiles[i].id
+//                        if (eID != mID) {
+//                            println("PlacedTiles with Index: $i does not have the same ID." +
+//                                    " ID of Entity Tile: $eID, ID of TurnMessage Tile: $mID")
+//                        }
+//                    }
+//                }
+//                // check TileStack/Supply
+//                val eTileStackSize = networkService.rootService.currentGame!!.currentTurn.
+//                                        gameField.tileStack.tiles.size
+//                val mTileStackSize = message.gameStateVerificationInfo.supply.size
+//                if (eTileStackSize != mTileStackSize) {
+//                    println("TileStack in Entity is not the same size as Supply in Received TurnMessage." +
+//                            " Size of Entity TileStack: $eTileStackSize, " +
+//                            "Size of TurnMessage TileStack: $mTileStackSize")
+//                } else {
+//                    for (i in 0 until mTileStackSize) {
+//                        val eStackID = networkService.rootService.currentGame!!.currentTurn.
+//                                        gameField.tileStack.tiles[i].id
+//                        val mStackID = message.gameStateVerificationInfo.supply[i]
+//                        if (eStackID != mStackID) {
+//                            println("TileID in TileStack does not equal ID in Received TurnMessage. " +
+//                                    "Index: $i, Entity ID: $eStackID, TurnMessage ID: $mStackID")
+//                        }
+//                    }
+//                }
+//                // check playerScores
+//                val ePlayers = networkService.rootService.currentGame!!.currentTurn.players
+//                for (i in 0 until ePlayers.size) {
+//                    if (ePlayers[i].score != message.gameStateVerificationInfo.playerScores[i]) {
+//                        println("PlayerScore of ${ePlayers[i].name} " +
+//                                "does not equal score from Received TurnMessage. " +
+//                                "Entity Score: ${ePlayers[i].score}," +
+//                                " TurnMessage Score: ${message.gameStateVerificationInfo.playerScores[i]}")
+//                    }
+//                }
+//
+//                // placeTile
+//                networkService.rootService.playerActionService.
+//                    placeTile(!message.fromSupply, message.posX,
+//                        message.posY, message.rotation/90, true)
+//
+//            } else {
+//                println("Received TurnMessage from $sender, even though it is not their turn.")
+//            }
+//            //next player gets called by playerActionServices
+//        }
+//    }
 
 
 }
